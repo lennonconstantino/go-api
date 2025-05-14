1d212c4d1bffc89f7498164e7c926408633779fe
lennonconstantino <lennonconstantino@gmail.com>
2025-05-14 20:06:46 -0300

    remove mock from controller

--- controller/product_controller.go
+++ controller/product_controller.go
@@ -1,7 +1,6 @@
 package controller
 
 import (
-	"go-api/model"
 	"go-api/usecase"
 	"net/http"
 
@@ -20,12 +19,9 @@ func NewProductController(usecase usecase.ProductUsecase) productController {
 }
 
 func (p *productController) GetProducts(ctx *gin.Context) {
-	products := []model.Product{
-		{
-			ID:    1,
-			Name:  "Batata frita",
-			Price: 20,
-		},
+	products, err := p.ProductUsecase.GetProducts()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, err)
 	}
 
 	ctx.JSON(http.StatusOK, products)
