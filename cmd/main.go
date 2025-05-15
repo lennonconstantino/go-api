ca9882336e84456e997026ef39fa0865eb0be50a
lennonconstantino <lennonconstantino@gmail.com>
2025-05-14 21:39:36 -0300

    fixing the route GetProductById in the main function

--- cmd/main.go
+++ cmd/main.go
@@ -34,7 +34,7 @@ func main() {
 
 	server.GET("/products", productController.GetProducts)
 	server.POST("/product", productController.CreateProduct)
-	server.GET("/product/:productID:", productController.GetProductById)
+	server.GET("/product/:productId", productController.GetProductById)
 
 	server.Run(":8000")
 }
