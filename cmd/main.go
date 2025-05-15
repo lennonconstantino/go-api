56c27ea9ba7157c17dbedd261b5034ac05a462bc
lennonconstantino <lennonconstantino@gmail.com>
2025-05-14 21:37:18 -0300

    creating the GetProductById

--- cmd/main.go
+++ cmd/main.go
@@ -34,6 +34,7 @@ func main() {
 
 	server.GET("/products", productController.GetProducts)
 	server.POST("/product", productController.CreateProduct)
+	server.GET("/product/:productID:", productController.GetProductById)
 
 	server.Run(":8000")
 }
